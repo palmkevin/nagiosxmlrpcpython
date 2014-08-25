358d0e043a20c55a356d39a0747bae58db87f6b5
Palm Kevin <palm.kevin@gmail.com>
2014-08-25 11:11:17 +0200

    enhanced help and error handling

--- nagiosclient.py
+++ nagiosclient.py
@@ -2,11 +2,8 @@
 
 # CAUTION: This script should be Python 2 and 3 compatible
 import sys
-
-NAGIOS_OK = 0
-NAGIOS_WARN = 1
-NAGIOS_CRIT = 2
-NAGIOS_UNKNOWN = 3
+import ssl
+import socket
 
 if sys.version_info >= (3, 0):
     # Python 3
@@ -14,29 +11,46 @@ if sys.version_info >= (3, 0):
 else:
     from xmlrpclib import ServerProxy, Fault as xmlrpcFault
 
+NAGIOS_OK = 0
+NAGIOS_WARN = 1
+NAGIOS_CRIT = 2
+NAGIOS_UNKNOWN = 3
+
+def abort(msg):
+    print(msg)
+    exit(NAGIOS_UNKNOWN)
+
 def main(argv):
+    if len(argv) > 0 and argv[0] in ('--help', '-h'):
+        abort("Usage: nagiosclient.py url_of_xmlrpc_server remote_method_to_be_called [arg1] [arg2] [arg3] [arg4]...")
     if len(argv) < 2:
-        raise Exception('Wrong arguments provided: At least the URL of the xmlrpc-server and the method should be passed')
+        abort("Invalid arguments provided. Try `nagiosclient.py --help` for more information")
     
     url = argv[0]
     methodName = argv[1]
     methodArgs = argv[2:]
+        
     try:
-        proxy = ServerProxy(url)
+        try:
+            proxy = ServerProxy(url)
+        except Exception as ex:
+            abort("Invalid URL for XMLRPC-server passed: %s" % ex)
+        
         try:
             ret = getattr(proxy, methodName)(*methodArgs)
         except xmlrpcFault as xf:
             fctNotSupMsg = 'method "%s" is not supported' % methodName
             if fctNotSupMsg in str(xf):
-                print("The remote method '%s' is unknown" % methodName)
-                exit(NAGIOS_UNKNOWN)
-            raise Exception(xf.faultString)
+                abort("The remote method '%s' is unknown" % methodName)
+            abort(xf.faultString)
+        except socket.error as sockerr:
+            abort("Socket error (%s): %s" % (type(sockerr).__name__, sockerr.args[1]))
         
         if not isinstance(ret, list):
             ret = [ret]
 
         if len(ret) > 2:
-            raise Exception("The remote method '%s' returned %s arguments. This is not supported" % (methodName, len(ret)))
+            abort("The remote method '%s' returned %s arguments. This is not supported (maximum 2 are allowed)" % (methodName, len(ret)))
         if len(ret) > 1:
             status, message = ret
         else:
@@ -49,19 +63,16 @@ def main(argv):
                     status = NAGIOS_UNKNOWN
                     message = "The remote method '%s' returned an empty answer" % methodName
                 else:
-                    status = NAGIOS_OK # We suppose that the client would raise an exception in all other cases
+                    status = NAGIOS_OK # if only a message is returned we suppose that everythis is ok
                     message = ret[0]
 
         if status in (NAGIOS_OK, NAGIOS_WARN, NAGIOS_CRIT, NAGIOS_UNKNOWN):
             print(message)
             exit(status)
         else:
-            print("XMLRPC-server returned invalid status: %s" % status)
-            exit(NAGIOS_UNKNOWN)
+            abort("XMLRPC-server returned invalid status: %s" % status)
     except Exception as ex:
-        print(str(ex))
-        exit(NAGIOS_UNKNOWN)
-
+        abort(str(ex))
 
 if __name__ == "__main__":
   main(sys.argv[1:])
